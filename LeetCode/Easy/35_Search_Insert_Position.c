193243f3961f335a31f6fde95efbf2667285ef47
Kagurazaka-Chiaki <46296322+Kagurazaka-Chiaki@users.noreply.github.com>
2020-01-06 22:20:22 +0800

    Summary-2020-01-06

---
+++ LeetCode/Easy/35_Search_Insert_Position.c
@@ -0,0 +1,18 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int pos = 0;
+    int flag1 = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == target) {
+            pos = i;
+            flag1 = 1;
+        }
+    }
+    if (flag1 == 0) {
+        for (int i = 0; i < numsSize; i++) {
+            if (nums[i] < target) {
+                pos = i + 1;
+            }
+        }
+    }
+    return pos;
+}
